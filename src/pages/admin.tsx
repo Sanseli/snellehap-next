dcfc0a1c161edcb03140c13e57b54e8de26a21d7
Sanseli <lisasansen97@gmail.com>
2023-03-29 19:59:46 +0300

    log in to admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,12 +1,38 @@
 import { useState, useEffect } from 'react'
+import loginService from '../services/login'
+import menuService from '../services/menus'
 
 export default function Admin () {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState('')
+
+    const handleLogin = async (event) => {
+		event.preventDefault()
+
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			})
+
+			window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+
+			menuService.setToken(user.token)
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			setNotification('verkeerde gebruikersnaam of wachtwoord')
+			setTimeout(() => {
+				setNotification('')
+			}, 5000)
+		}
+	}
 
     const loginForm = () => (
-        <form className="space-y-2">
+        <form className="space-y-2" onSubmit={handleLogin}>
             <span className="mr-2">username</span>
             <input 
                 type="string" 
@@ -26,12 +52,13 @@ export default function Admin () {
             />
             <br/>
             <button className=" bg-slate-800">Log in</button>
+            <div className='text-red-400'>{notification}</div>
         </form>
     )
 
     return <main>
         <div className="p-20">
-            {loginForm()}
+            {user === null ? loginForm() : <div></div>}
         </div>
     </main>
 }
